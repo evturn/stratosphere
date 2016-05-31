5d5f224b830d8d3509a9ccd0a18702a86683d5ab
Evan Turner <evturn@gmail.com>
2016-05-31 13:55:48 -0400

    Add Image and assets to platforms

--- Stratosphere.js
+++ Stratosphere.js
@@ -4,7 +4,8 @@ import {
   StyleSheet,
   Text,
   TextInput,
-  View
+  View,
+  Image
 } from 'react-native'
 import Forecast from './Forecast'
 
@@ -23,7 +24,7 @@ class Stratosphere extends Component {
   }
 
   handleTextChange(e) {
-    console.log(e.nativeEvent.text);
+    console.log(e.nativeEvent.text)
     this.setState({
       zip: e.nativeEvent.text
     })
@@ -32,53 +33,76 @@ class Stratosphere extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Náive!
-        </Text>
-        <Forecast
-          main={this.state.forecast.main}
-          description={this.state.forecast.description}
-          temp={this.state.forecast.temp}
-        />
-        <Text style={styles.welcome}>
-          The weather is terrible around {this.state.zip}.
-        </Text>
-        <TextInput
-          style={styles.input}
-          returnKeyType='go'
-          onChange={e => this.handleTextChange(e)} />
-        <Text style={styles.instructions}>
-          Shake it and rub it down for dev menu
-        </Text>
+        <Image
+          source={require('image!sky')}
+          resizeMode='cover'
+          style={styles.backdrop}>
+          <View style={styles.overlay}>
+            <View style={styles.row}>
+              <Text style={styles.mainText}>
+                Stratosphere: Where weather is, also.
+              </Text>
+              <View style={styles.zipContainer}>
+                <TextInput
+                  style={styles.input}
+                  returnKeyType='go'
+                  onSubmitEditing={e => this.handleTextChange(e)}
+                />
+              </View>
+            </View>
+            <Forecast
+              main={this.state.forecast.main}
+              description={this.state.forecast.description}
+              temp={this.state.forecast.temp}
+            />
+          </View>
+        </Image>
       </View>
-    );
+    )
   }
 }
 
+const baseFontSize = 16
 const styles = StyleSheet.create({
-  input: {
-    fontSize: 20,
-    borderWidth: 2,
-    height: 40,
-    borderColor: 'transparent',
-    textAlign: 'center'
-  },
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#c1c1c1',
+    paddingTop: 30
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  backdrop: {
+    flex: 1,
+    flexDirection: 'column'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#ffffff',
-    marginBottom: 5,
+  overlay: {
+    paddingTop: 5,
+    backgroundColor: '#000000',
+    opacity: 0.5,
+    flexDirection: 'column',
+    alignItems: 'center'
   },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    alignItems: 'flex-start',
+    padding: 30
+  },
+  zipContainer: {
+    flex: 1,
+    borderBottomColor: '#DDDDDD',
+    borderBottomWidth: 1,
+    marginLeft: 5,
+    marginTop: 3
+  },
+  zipCode: {
+    width: 50,
+    height: baseFontSize,
+  },
+  mainText: {
+    flex: 1,
+    fontSize: baseFontSize,
+    color: '#FFFFFF'
+  }
 })
 
 module.exports = Stratosphere
